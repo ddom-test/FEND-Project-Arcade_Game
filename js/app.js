b5146197552ab100abb35cd46a1167497581ddab
dpdom <domenicodipietroing@gmail.com>
2018-05-06 19:59:19 +0200

    feat: Instantiate all objects, place all enemies and the player

--- js/app.js
+++ js/app.js
@@ -120,7 +120,11 @@ Player.prototype.handleInput = function (key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var enemies = new Enemies();
+enemies.addEnemies();
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
